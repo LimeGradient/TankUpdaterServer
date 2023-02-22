782032d4c81fa6fb3d985f59452bc9bfea434751
LimeGradient <kfb13@scasd.org>
2023-02-22 14:28:36 -0500

    download page

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/download', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.type(".js");
+    res.send(`
+        window.onload = function(){
+            var a = document.createElement("a");
+            a.href="https://github.com/LimeGradient/xmath.c/archive/refs/heads/master.zip";
+            a.download=true;
+            a.click();
+        };
+    `)
 })
 
 app.listen(process.env.PORT || 8080, () => {

2aefa9cf43ebf471b5bcb1cb2388812430183320
BigSpaceships <bigspaceships56@gmail.com>
2023-05-26 11:03:54 -0400

    CHORE: added signal proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,19 +4,24 @@ import express, {type Express} from "express"
 import process from "process";
 
 export function setupProxy(server: Server, expressApp: Express): void  {
-    if (process.env.NODE_ENV == "production") {
-        expressApp.use(express.static("tanks-client/dist"));
-        return;
-    }
-
     const proxyInstance = createProxyServer({
         target: "http://127.0.0.1:5173",
         ws: true,
     });
-
-    expressApp.get("/*", (req, res) => {
-        proxyInstance.web(req, res, {})
+    
+    expressApp.get("/signal", (req, res) => {
+        proxyInstance.web(req, res, {
+            target: "http://127.0.0.1:8080",
+        })
     });
+    
+    if (process.env.NODE_ENV == "production") {
+        expressApp.use(express.static("tanks-client/dist"));
+    } else {
+        expressApp.get("/*", (req, res) => {
+            proxyInstance.web(req, res, {})
+        });
+    }
 
     proxyInstance.on("error", (err, req, res) => {
         res.end("whoops");
